df9b06c8e0db58d573f8e4b32b3ca84cb7012804
Shelby Simpson <shelbycam78@gmail.com>
2021-06-15 19:02:25 -0400

    Init Commit Poco setup

---
+++ DevTeamMgmtApp.poco/DevTeamPoco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamMgmtApp.poco
+{
+    class DevTeamPoco
+    {
+        public string Developers { get; set; }
+
+        public string TeamNames { get; set; }
+
+        public string TeamId { get; set; }
+
+
+    }
+}
---
+++ DevTeamMgmtApp.poco/DeveloperPoco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamMgmtApp.poco
+{
+    class DeveloperPoco
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ID { get; set; }
+
+        public string DevTeam { get; set; }
+
+        public bool AccessToPluralSight { get; set; }
+
+         
+    }
+}
